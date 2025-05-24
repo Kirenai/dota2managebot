f6e1b15c04679a4f8fe23c6473f500c50bb7e640
Kirenai <pool_dota2@hotmail.com>
2025-05-24 01:47:56 -0500

    init commit

---
+++ src/main/kotlin/me/kirenai/dota2managebot/Dota2ManageBotApplication.kt
@@ -0,0 +1,11 @@
+package me.kirenai.dota2managebot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Dota2ManageBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<Dota2ManageBotApplication>(*args)
+}
---
+++ src/test/kotlin/me/kirenai/dota2managebot/Dota2managebotApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kirenai.dota2managebot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Dota2managebotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
